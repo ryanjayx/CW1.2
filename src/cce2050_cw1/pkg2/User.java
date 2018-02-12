594bb3526df6b6003eb35fbf6efc52e9aa3b3863
Lenovo-PC <Lenovo-PC@LAPTOP-PSBBEHLG>
2018-02-12 23:55:19 +0400

    User without sync

---
+++ src/cce2050_cw1/pkg2/User.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cce2050_cw1.pkg2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lenovo-PC
+ */
+public class User {
+    
+ private String name;
+ private String surname;
+ private BankAccount bankAccount;
+ private ArrayList<Double> transactionlist;
+ public User(String n, String s, BankAccount bA, ArrayList<Double> tl)                                                       //Constructor for class User
+    {
+        this.name = n;
+        this.surname = s;
+        this.bankAccount = bA;
+        this.transactionlist=tl;
+    }
+    public String getUserName(){
+        return this.name+" "+this.surname;
+    }
+ 
+
+ 
+ 
+ 
+}
