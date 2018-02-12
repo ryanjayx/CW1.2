d2c20ad1b0a857764dc06b8f6fdd388382c521e0
Lenovo-PC <Lenovo-PC@LAPTOP-PSBBEHLG>
2018-02-13 00:39:46 +0400

    bankAccount without sync

---
+++ src/cce2050_cw1/pkg2/bankAccount.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cce2050_cw1.pkg2;
+
+/**
+ *
+ * @author Lenovo-PC
+ */
+class bankAccount {
+    private long accountNo;                                                                     
+    private double accountBalance;
+    private User user;
+    public bankAccount(long accountNo, double accountBalance)                                       
+    {
+        this.accountNo = accountNo;
+        this.accountBalance = accountBalance;  
+    }
+    public long getAccountNo()
+    {
+        return this.accountNo;
+    }
+    public double getAccountBalance()
+    {
+        return this.accountBalance;
+    }   
+    public void deposit(double value,String name)                                               
+    {
+        System.out.println("The user-"+name+" deposits an amount of "+value);
+        accountBalance+=value;
+    }
+    public void withdraw(double value,String name)
+    {
+        System.out.println("The user-"+name+" withdraws an amount of "+Math.abs(value));
+        accountBalance-=value;
+    }
+}
+    
+
